e84f7c551e7a55a347d846a93a836e8d768effe6
yutateru6-collab <yutateru6@gmail.com>
2026-09-08 22:19:47 +0900

    feat: mount notes school calendar shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import AppShell from './AppShell.tsx';
 import './index.css';
+import './app-shell.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppShell />
   </StrictMode>,
 );
